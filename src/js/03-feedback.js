9d94eaff934982917570f019c35fe7f0c4ef63b3
rango198 <137562811+rango198@users.noreply.github.com>
2023-11-06 21:09:09 +0200

    rework

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,41 +1,42 @@
 import throttle from 'lodash.throttle';
 import { saveToLS, loadFromLS } from './helpers';
+const LOCAL_KEY = 'feedback-form-state';
 
 const refs = {
   formEL: document.querySelector('.feedback-form'),
-  emailInput: document.querySelector('input[name="email"]'),
-  messageInput: document.querySelector('textarea[name="message"]'),
 };
 
 refs.formEL.addEventListener('input', throttle(saveFormState, 500));
 refs.formEL.addEventListener('submit', submitForm);
 
-function saveFormState() {
-  const formStateSave = {
-    email: refs.emailInput.value.trim(),
-    message: refs.messageInput.value.trim(),
-  };
-  saveToLS('feedback-form-state', formStateSave);
+let formData = {};
+
+function saveFormState(event) {
+  formData[event.target.name] = event.target.value.trim();
+  saveToLS(LOCAL_KEY, formData);
 }
 
 function onLoad() {
-  const formStateLoad = loadFromLS('feedback-form-state');
-  if (formStateLoad) {
-    refs.emailInput.value = formStateLoad.email;
-    refs.messageInput.value = formStateLoad.message;
+  let data = localStorage.getItem(LOCAL_KEY);
+  if (!data) return;
+  try {
+    formData = JSON.parse(data);
+    Object.entries(formData).forEach(([key, val]) => {
+      if (refs.formEL.elements[key]) {
+        refs.formEL.elements[key].value = val;
+      }
+    });
+  } catch (error) {
+    console.error('Error parsing JSON data:', error);
   }
 }
 
 function submitForm(event) {
   event.preventDefault();
-  const dataForm = {
-    email: refs.emailInput.value.trim(),
-    message: refs.messageInput.value.trim(),
-  };
-  console.log(dataForm);
-  localStorage.removeItem('feedback-form-state');
-  refs.emailInput.value = '';
-  refs.messageInput.value = '';
+  console.log(formData);
+  localStorage.removeItem(LOCAL_KEY);
+  event.currentTarget.reset();
+  formData = {};
 }
 
 onLoad();
